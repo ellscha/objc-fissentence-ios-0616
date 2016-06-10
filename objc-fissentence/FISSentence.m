d4d56f4c5d3fa6cc808c7fe5d5d3f283b44764c3
ellscha <escharlin@wesleyan.edu>
2016-06-10 13:53:04 -0400

    all tests are passing

--- objc-fissentence/FISSentence.m
+++ objc-fissentence/FISSentence.m
@@ -19,9 +19,7 @@
         
         [self.words addObject:word];
     }
-    else{
-        [self.words addObject:nil];
-    }
+    
     
     [self assembleSentence];
 }
@@ -29,48 +27,80 @@
 
 -(void)addWords:(NSArray *)words
 withPunctuation:(NSString *)punctuation{
-    
+    if ([words count] > 0) {
+        if ([self validPunctuation:punctuation]) {
+            self.punctuation = punctuation;
+            
+            
+            for (NSString *word in words) {
+                if ([self validWord:word]) {
+                    [self.words addObject:word];
+                }
+            }
+        }
+    }
+    [self assembleSentence];
 }
 
 -(void)removeWordAtIndex:(NSUInteger)index{
-    [self.words removeObjectAtIndex:index];
-    
+    if ([self validIndex:index]) {
+        
+        [self.words removeObjectAtIndex:index];
+    }
+    [self assembleSentence];
 }
 
 -(void)insertWord:(NSString *)word
           atIndex:(NSUInteger)index{
+    if ([self validWord:word]&&[self validIndex:index]) {
+        [self.words insertObject:word atIndex:index];
+    }
+    [self assembleSentence];
+    
     
 }
 
 -(void)replacePunctuationWithPunctuation:(NSString *)punctuation{
-    
+    if ([self validPunctuation:punctuation]) {
+        self.punctuation = punctuation;
+    }
+    [self assembleSentence];
 }
 
 -(void)replaceWordAtIndex:(NSUInteger)index
                  withWord:(NSString *)word{
+    if ([self validWord:word]) {
+        if ([self validIndex:index]) {
+            [self.words replaceObjectAtIndex:index withObject:word];
     
+        }
+    }
+    [self assembleSentence];
 }
 -(void)assembleSentence{
     self.sentence = [self.words componentsJoinedByString:@" "];
+    self.sentence = [self.sentence stringByAppendingString:self.punctuation];
 }
 
 -(BOOL)validWord:(NSString *)word{
     
-    if ([word isEqualToString:@""]||[word isEqualToString:@" "]) {
-        if ([word isEqual:nil]) {
-            return NO;
-        }
+    if ([word isEqualToString:@""]||[word isEqualToString:@" "]||!word) {
+        return NO;
+        
     }
     return YES;
 }
 
 -(BOOL)validPunctuation:(NSString *)punctuation{
-    
+    NSArray *validPunctuation = @[@".",@"?",@"!",@";",@":",@"—"];
+    if ([validPunctuation containsObject:punctuation]) {
+        return YES;
+    }
     return NO;
 }
 
 -(BOOL)validIndex:(NSUInteger)index{
-    if (index > self.sentence.length) {
+    if (index >= [self.words count]) {
         return NO;
     }
     return YES;
